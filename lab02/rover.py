836f296ab5891fdba16bc951f3c2543054113e5d
Jack Mullane <Jack.Mullane1@marist.edu>
2019-09-12 15:10:48 -0400

    The solution to the curiosity rover exercise

--- lab02/rover.py
+++ lab02/rover.py
@@ -2,5 +2,5 @@ def main():
     dist = 34000000
     speed = 186000
     time = dist/speed
-    print("It takes", time, "seconds for NASA to recieve a photo from the rover on mars.")
+    print("It takes", time, "seconds for NASA to recieve a photo from the Curiosity rover on mars.")
 main()
