2285126685093d6f070b31b14852ae581eb20704
Jack Mullane <Jack.Mullane1@marist.edu>
2019-09-18 23:49:17 -0400

    Solution to Mad Libs exercise.

---
+++ lab02/madlib.py
@@ -0,0 +1,9 @@
+def main():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    place = input("Enter a place: ")
+    adj = input("Enter an adjective: ")
+
+    print(noun, verb, place, adj)
+    
+main()
