d3d94a36bc2a3c6f117d308dba0c68e8ebf8e935
Jack Mullane <Jack.Mullane1@marist.edu>
2019-09-19 14:53:41 -0400

    The solution to the fibonacci sequence.

---
+++ lab03/fibonacci.py
@@ -0,0 +1,18 @@
+def main():
+    num = int(input("Enter a number for the fibonnaci sequence: "))
+    result = fibonacci(num)
+    print(result)
+
+
+def fibonacci(num):
+    if num < 0:
+        return
+    elif num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+main()
