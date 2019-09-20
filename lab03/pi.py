e6d068f7dd3621417aea84a71b64ef66beec5ab1
Jack Mullane <Jack.Mullane1@marist.edu>
2019-09-19 21:38:07 -0400

    Wrote programs to compute Fibonacci and Pi.

---
+++ lab03/pi.py
@@ -0,0 +1,28 @@
+import math
+
+
+def main():
+    num = int(input("Enter the nth term of the pi sequence: "))
+    result = (pi(num))
+    print("Sum: ", result)
+    print("Accuracy: ", (math.pi - result))
+
+
+def pi(num):
+    if num <= 0:
+        print("The number has to be positive")
+    elif num % 2 == 0:
+        num *= 2
+    else:
+        num *= 2
+        num -= 1
+    add = 0
+    for i in range(1, num, 4):
+        now = (4/i)
+        add += now
+        after = (4/(i+2))
+        add -= after
+    return add
+
+
+main()
