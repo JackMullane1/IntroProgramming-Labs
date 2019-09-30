35872744944ea9a8ec7cca45e0b26f9287dcbd1b
Jack Mullane <Jack.Mullane1@marist.edu>
2019-09-30 17:48:34 -0400

    Refined the guessing game. Closes #2. Closes #3.

--- lab04/guessing_game.py
+++ lab04/guessing_game.py
@@ -1,11 +1,16 @@
 # A guessing game made with a loop and conditional statements.
 def main():
     animal = "cat"
-    guess = input("Try to guess what animal I'm thinking of: ")
-    while guess != animal:
-        guess = input("Incorrect, guess again: ")
-    else:
-        print("Congrats! It was a(n)", animal + "!")
+    guess = input("Try to guess what animal I'm thinking of: ").lower()
+    correct = 0
+    while correct < 1:
+        if guess == animal:
+            correct += 1
+            print("Congrats! It was a(n)", animal + "!")
+        elif guess == "quit":
+            return
+        else:
+            guess = input("Try again (or type quit to quit): ").lower()
+
 
 main()
-
