d7a18946dedde0f965f365881a8b8743c23693b5
Jack Mullane <Jack.Mullane1@marist.edu>
2019-09-30 18:23:02 -0400

    Further refined the guessing game. Closes #4 Closes #5

--- lab04/guessing_game.py
+++ lab04/guessing_game.py
@@ -4,13 +4,19 @@ def main():
     guess = input("Try to guess what animal I'm thinking of: ").lower()
     correct = 0
     while correct < 1:
-        if guess == animal:
-            correct += 1
-            print("Congrats! It was a(n)", animal + "!")
-        elif guess == "quit":
+        if "q" == guess[:1]:
             return
+        elif guess == animal:
+            correct += 1
+            answer = input(
+                "You got it right! So do you like " + str(animal) +
+                '''s? (Type y for yes, n for no): ''').lower()
+            if answer == "y":
+                print("That's great! So do I!")
+            else:
+                print("What did", animal + "s do to you huh?")
         else:
-            guess = input("Try again (or type quit to quit): ").lower()
+            guess = input("Try again (or type q to quit): ").lower()
 
 
 main()
