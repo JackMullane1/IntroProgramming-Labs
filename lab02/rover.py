d953157c4d9de1983a19ce01eef9253241580d33
Jack Mullane <Jack.Mullane1@marist.edu>
2019-10-03 12:46:46 -0400

    Fixed the Rover exercise based on pycodestyle.

--- lab02/rover.py
+++ lab02/rover.py
@@ -2,7 +2,8 @@ def main():
     dist = 34000000
     speed = 186000
     time = dist/speed
-    print("It takes", time, "seconds for NASA to recieve a photo from the Curiosity rover on mars.")
+    print("It takes", time, '''seconds for NASA to recieve a photo from the
+    Curiosity rover on mars.''')
 
 
 main()
