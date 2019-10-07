122df5ccf79352f06d9f4f603a80f2607f4bdfac
Jack Mullane <Jack.Mullane1@marist.edu>
2019-10-07 12:50:48 -0400

    The finished program to generate Marist emails.

--- lab05/credentials.py
+++ lab05/credentials.py
@@ -4,38 +4,46 @@
 # Created: 2019-10-03
 
 
-def main():
-    userN = maristUname(names())
-    passW = password()
-    print(
-        '''Congratulations, you have created a new Marist account!
-        Your new email is:''', userN, "\n")
-    print("Keep this password a secret: ", passW, "\n")
-
 def names():
     print("\n")
-    first = input("Enter your first name: ")
+    first = input("Enter your first name: ").lower()
     print("\n")
-    last = input("Enter your last name: ")
+    last = input("Enter your last name: ").lower()
     print("\n")
     fullName = [first, last]
     return fullName
 
-def maristUname(fullName):
-    uName = str(fullName[0] + "." + fullName[1] + "1@marist.edu")
+
+def maristUname(names):
+    uName = str(names[0] + "." + names[1] + "1@marist.edu")
     return uName
 
+
 def password():
-    passwd = input("Create a new password: ")
-    print("\n") 
-    while len(passwd) < 8:
+    newPassword = input("Create a new password: ")
+    print("\n")
+    return passStrength(newPassword)
+
+
+def passStrength(password):
+    passFilter = password
+    while len(passFilter) < 8:
         print("Fool of a Took! That password is feeble!\n")
-        passwd = input("Create a better password: ")
+        passFilter = input("Create a better password: ")
         print("\n")
     else:
         print("The force is strong in this one...")
         print("\n")
-        return passwd
+        return passFilter
+
+
+def main():
+    userName = maristUname(names())
+    userPass = password()
+    print(
+        '''Congratulations, you have created a new Marist account!
+        Your new email is:''', userName, "\n")
+    print("Keep this password a secret: ", userPass, "\n")
 
 
 main()
