dbeb0cd6ead33258d1d4a06d5eabcdc6ebfe4187
Jack Mullane <Jack.Mullane1@marist.edu>
2019-10-07 12:59:48 -0400

    Added better documentation.

--- lab02/madlib.py
+++ lab02/madlib.py
@@ -1,3 +1,9 @@
+# CMPT 120
+# Lab 2: Joins strings together based on user input.
+# Jack Mullane
+# 10/7/2019
+
+
 def main():
     noun = input("Enter a noun: ")
     verb = input("Enter a verb: ")
