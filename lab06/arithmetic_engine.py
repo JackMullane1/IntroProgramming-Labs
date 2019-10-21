20a31f9747c7812882aebf97a449b7fe04dd870f
Jack Mullane <Jack.Mullane1@marist.edu>
2019-10-21 13:31:49 -0400

    Completed the first part to the Arithmetic problem.

---
+++ lab06/arithmetic_engine.py
@@ -0,0 +1,42 @@
+# CMPT 120 - Lab #6
+# Jack Mullane
+# 10-17-2019
+
+def show_intro():
+    print("Welcome to the Arithmetic Engine!")
+    print("=================================\n")
+    print("Valid commands are: 'add', 'sub', 'mult', 'div', and 'quit'.\n")
+
+def show_outro():
+    print("\nThank you for using the Arithmetic Engine...")
+    print("\nPlease come back again soon!")
+
+def do_loop():
+    supported = ["add", "sub", "mult", "div", "quit"]
+    while True:
+        cmd = str(input("What computation do you want to perform?: "))
+        if cmd.lower() in supported:
+            if cmd == "quit":
+                break
+            else:
+                num1 = int(input("Enter the first number: "))
+                num2 = int(input("Enter the second number: "))
+                if cmd == "add":
+                    result = num1 + num2
+                elif cmd == "sub":
+                    result = num1 - num2
+                elif cmd == "mult":
+                    result = num1 * num2
+                elif cmd == "div":
+                    result = num1 / num2
+                print("The result is:", result, "\n")
+        else:
+            print("That command is not supported.\n")
+
+def main():
+    show_intro()
+    do_loop()
+    show_outro()
+
+main()
+    
