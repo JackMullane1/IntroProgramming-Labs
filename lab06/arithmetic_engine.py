1f67f99aa7a131d2fc66abc1d1e5342c4c16d94d
Jack Mullane <Jack.Mullane1@marist.edu>
2019-10-21 13:47:15 -0400

    Completed the full arithmetic problem.

--- lab06/arithmetic_engine.py
+++ lab06/arithmetic_engine.py
@@ -2,14 +2,17 @@
 # Jack Mullane
 # 10-17-2019
 
+
 def show_intro():
-    print("Welcome to the Arithmetic Engine!")
+    print("\nWelcome to the Arithmetic Engine!\n")
     print("=================================\n")
     print("Valid commands are: 'add', 'sub', 'mult', 'div', and 'quit'.\n")
 
+
 def show_outro():
     print("\nThank you for using the Arithmetic Engine...")
-    print("\nPlease come back again soon!")
+    print("\nPlease come back again soon!\n")
+
 
 def do_loop():
     supported = ["add", "sub", "mult", "div", "quit"]
@@ -19,8 +22,12 @@ def do_loop():
             if cmd == "quit":
                 break
             else:
-                num1 = int(input("Enter the first number: "))
-                num2 = int(input("Enter the second number: "))
+                try:
+                    num1 = int(input("Enter the first number: "))
+                    num2 = int(input("Enter the second number: "))
+                except:
+                    print("You have to enter a number!\n")
+                    continue
                 if cmd == "add":
                     result = num1 + num2
                 elif cmd == "sub":
@@ -29,14 +36,15 @@ def do_loop():
                     result = num1 * num2
                 elif cmd == "div":
                     result = num1 / num2
-                print("The result is:", result, "\n")
+                print("\nThe result is:", result, "\n")
         else:
             print("That command is not supported.\n")
+
 
 def main():
     show_intro()
     do_loop()
     show_outro()
 
+
 main()
-    
